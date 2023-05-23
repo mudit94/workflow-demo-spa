2c68dd0bc192206b00c1a3a00911e576f790b14d
mudit94 <mudit94@gmail.com>
2023-05-23 19:49:44 +0530

    test with new config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import styles from '../styles/helloName.module.css';
 export async function getServerSideProps() {
   const name = process.env.NAME || 'Default Name';
   const test = process.env.TEST || 'Default Test';
-  return { props: { name, test } };
+    const testbymudit = process.env.MUDITTEST || 'Default Test';
+  return { props: { name, test,testbymudit } };
 }
 
-export default function Index({ name, test }: { name: string, test: string }) {
+export default function Index({ name, test,testbymudit }: { name: string, test: string,testbymudit: string }) {
   return <div className={styles.center}>
     <HelloName name={`NAME : ${name}`} />
     <HelloName name={`TEST : ${test}`} />
+    <HelloName name={`MUDITTEST : ${testbymudit}`} />
   </div>;
 }
